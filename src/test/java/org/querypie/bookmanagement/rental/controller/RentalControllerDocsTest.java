6cf357bb4dbdb99fb53fbb01510c220cab9d77e5
NaMinhyeok <nmh9097@gmail.com>
2025-02-14 15:54:14 +0900

    test: 도서 반납 컨트롤러 테스트 추가

--- src/test/java/org/querypie/bookmanagement/rental/controller/RentalControllerDocsTest.java
+++ src/test/java/org/querypie/bookmanagement/rental/controller/RentalControllerDocsTest.java
@@ -6,12 +6,14 @@ import com.epages.restdocs.apispec.SimpleType;
 import org.junit.jupiter.api.Test;
 import org.querypie.bookmanagement.rental.controller.request.RentalBookRequestDto;
 import org.querypie.bookmanagement.rental.service.command.RentalBookCommand;
+import org.querypie.bookmanagement.rental.service.command.ReturnBookCommand;
 import org.querypie.bookmanagement.support.ControllerTestSupport;
 import org.springframework.http.MediaType;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+import static com.epages.restdocs.apispec.ResourceDocumentation.parameterWithName;
 import static com.epages.restdocs.apispec.ResourceDocumentation.resource;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.willDoNothing;
@@ -55,5 +57,35 @@ class RentalControllerDocsTest extends ControllerTestSupport {
             );
     }
 
+    @Test
+    void 책을_반납한다() throws Exception {
+        willDoNothing().given(rentalService).returnBooks(any(ReturnBookCommand.class), any(LocalDateTime.class));
+
+        mockMvc.perform(post("/api/v1/rentals/{rentalId}/books/return", 1L)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(new RentalBookRequestDto(List.of(1L, 2L), 1L))
+                ))
+            .andExpect(status().isNoContent())
+            .andDo(document("return-book",
+                resource(
+                    ResourceSnippetParameters.builder()
+                        .description("책 반납")
+                        .tags("Rental")
+                        .pathParameters(
+                            parameterWithName("rentalId").description("대여 ID")
+                        )
+                        .requestFields(
+                            fieldWithPath("bookIds").description("반납할 책 ID 목록"),
+                            fieldWithPath("userId").description("반납자 ID")
+                        )
+                        .responseFields(
+                            fieldWithPath("result").type(SimpleType.STRING).description("결과"),
+                            fieldWithPath("data").ignored(),
+                            fieldWithPath("error").ignored()
+                        )
+                        .responseSchema(Schema.schema("returnBookResponse"))
+                        .build()
+                )));
+    }
 
 }
